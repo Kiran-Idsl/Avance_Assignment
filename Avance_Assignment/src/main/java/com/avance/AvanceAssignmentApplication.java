cf9d76faf96b55a023b5ee4777c674f4c4663bd9
HP <HP@192.168.0.168>
2023-06-27 16:45:44 +0530

    second

--- Avance_Assignment/src/main/java/com/avance/AvanceAssignmentApplication.java
+++ Avance_Assignment/src/main/java/com/avance/AvanceAssignmentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class AvanceAssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvanceAssignmentApplication.class, args); 
+		SpringApplication.run(AvanceAssignmentApplication.class, args);  
 	}
 
 }
